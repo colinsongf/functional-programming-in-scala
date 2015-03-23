0ac38aeb4889159b6e0667ea79903cb1950c8f98
mewwts <mats@plysjbyen.net>
2015-03-23 22:39:39 +0100

    Solution to 2.2

---
+++ 2-gettingstarted/2.2.scala
@@ -0,0 +1,11 @@
+object Sorted {
+  def isSorted[A](as: Array[A], gt: (A, A) => Boolean): Boolean = {
+    @annotation.tailrec
+    def go(n: Int) = {
+      if (n >= as.length) true
+      else if (gt(as(n), as(n+1)) false 
+      else go(n+1)
+    }
+    go(0);
+  }
+}
